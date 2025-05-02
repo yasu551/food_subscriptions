44c2dafeac04e0cee8a64fc7095c87db1b7c2f97
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2025-05-02 14:19:31 +0900

    プランを更新できるように

--- app/controllers/admins/plans_controller.rb
+++ app/controllers/admins/plans_controller.rb
@@ -1,8 +1,28 @@
 class Admins::PlansController < Admins::ApplicationController
+  before_action :set_plan, only: %i[edit update]
+
   def index
     @plans = Plan.default_order
   end
 
   def edit
+  end
+
+  def update
+    if @plan.update(plan_params)
+      redirect_to admins_plans_path, notice: 'プランを更新しました。', status: :see_other
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def set_plan
+    @plan = Plan.find(params[:id])
+  end
+
+  def plan_params
+    params.expect(plan: %i[name description])
   end
 end
