0199fa24b81a0819c439b3bbe7e2b7b8c7dfe876
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2025-05-02 16:30:28 +0900

    プランの詳細が見れるように

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,8 +1,16 @@
 class PlansController < ApplicationController
+  before_action :set_plan, only: %i[show]
+
   def index
     @plans = Plan.default_order
   end
 
   def show
+  end
+
+  private
+
+  def set_plan
+    @plan = Plan.find(params[:id])
   end
 end
