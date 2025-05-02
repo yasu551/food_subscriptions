3f87cb72cee69a848bad7c4dd79cbebb4ddf438c
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2025-05-02 16:07:21 +0900

    プランとかを設定できるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, authentication_keys: %i[login_key]
 
+  belongs_to :plan, optional: true
+
   validates :login_key, uniqueness: true
   validates :name, presence: true
 
