a2bbb0795f61b4732a1153e5aa70d31de9a69010
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2025-05-02 16:36:15 +0900

    配達サイクルを更新できるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  extend Enumerize
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, authentication_keys: %i[login_key]
 
   belongs_to :plan, optional: true
+
+  enumerize :delivery_cycle, in: %w[weekly bi_monthly]
 
   validates :login_key, uniqueness: true
   validates :name, presence: true
